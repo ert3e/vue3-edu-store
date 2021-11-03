971563d0793e9534daa67c55961e594f07c2531e
Dmitriy Samoilenko <googscomes@gmail.com>
2021-11-03 16:14:03 +0200

    add form valid

--- src/components/CartBillingForm.vue
+++ src/components/CartBillingForm.vue
@@ -4,29 +4,34 @@
     <div class="col-md-6">
       <label for="firstName" class="form-label">First Name</label>
       <input v-model="firstName" type="text" class="form-control" id="firstName" placeholder="First Name">
+      <span v-if="!errors.isFirstNameValid"> First Name error</span>
     </div>
     <div class="col-md-6">
       <label for="lastName" class="form-label">Last Name</label>
       <input v-model="lastName" type="text" class="form-control" id="lastName" placeholder="Last Name">
+      <span v-if="!errors.isLastNameValid"> Last Name error</span>
     </div>
     <div class="col-12">
       <label for="city" class="form-label">City</label>
       <input v-model="city" type="text" class="form-control" id="city" placeholder="City">
+      <span v-if="!errors.isCityValid"> City error</span>
     </div>
     <div class="col-12">
       <label for="address" class="form-label">Address</label>
       <input v-model="address" type="text" class="form-control" id="address" placeholder="Address">
+      <span v-if="!errors.isAddressValid"> Address error</span>
     </div>
     <div class="col-md-6">
       <label for="email" class="form-label">Email Address</label>
       <input v-model="email" type="email" class="form-control" id="email" placeholder="Email Address">
+      <span v-if="!errors.isCityValid"> Email error</span>
     </div>
     <div class="col-md-6">
       <label for="phone" class="form-label">Phone</label>
       <input v-model="phone"  type="text" class="form-control" id="phone" placeholder="+38(099) 999-99-99">
     </div>
     <div class="col-12 d-flex justify-content-end">
-      <button type="submit" class="btn btn-success">
+      <button v-bind:disabled="formValid" type="submit" class="btn btn-success">
         Place Order
       </button>
     </div>
@@ -34,6 +39,7 @@
 </template>
 
 <script>
+
 export default {
   name: "CartBillingForm",
   data: () => ({
@@ -43,6 +49,8 @@ export default {
     address: "",
     email: "",
     phone: "",
+    errors:{},
+    formValid: false
   }),
   methods: {
     checkOutForm(){
@@ -64,6 +72,34 @@ export default {
       this.email = "";
       this.phone = "";
       this.$root.cartProducts = [];
+    }
+  },
+  watch: {
+    firstName(newValue){
+      this.errors.isfirstNameValid = newValue.length > 0 &&  newValue.length <= 10;
+    },
+    lastName(newValue){
+      this.errors.isLastNameValid = newValue.length > 0 && newValue.length <= 10  ;
+    },
+    city(newValue){
+      this.errors.isCityValid = newValue.length > 0 && newValue.length <= 10;
+    },
+    address(newValue){
+      this.errors.isAddresValid = newValue.length > 0 && newValue.length <= 10;
+    },
+    email(newValue){
+      this.errors.isEmailValid = newValue.length > 0 && newValue.length <= 10;
+    },
+    phone(newValue){
+      this.errors.isPhoneValid = newValue.length > 0 && newValue.length <= 11;
+    },
+    errors(newValue){
+      for(let val in newValue){
+        if(!newValue[val]){
+          this.formValid = false;
+        }
+      }
+      this.formValid = true;
     }
   }
 }
